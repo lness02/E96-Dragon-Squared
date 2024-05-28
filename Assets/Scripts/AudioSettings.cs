6372abc4fd50b1aee10f488e311efc6b9b4097ca
katruong855 <katruong855@gmail.com>
2024-05-27 19:01:11 -0700

    Options, AudioSettings, and started CreditsMenu
    - Options menu fully functional
    -includes Audio and AudioSettings
    -Audio can now be adjusted from options menu via sliders, and carries over to each scene as long as Player starts from MainMenu
    -started CreditsMenu, not implemented yet

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -6,15 +6,40 @@ using UnityEngine.UI;
 public class AudioSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
-    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider musicSlider;
+
+    [SerializeField] private Slider SFXSlider;
 
     private void Start(){
-        SetSoundVolume();
+        if(PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume")){
+            LoadVolume();
+        }else{
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+        
     }
 
-    public void SetSoundVolume()
+
+    public void SetMusicVolume()
     {
-        float volume = soundSlider.value;
-        myMixer.SetFloat("sound", Mathf.Log10(volume)*20);
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        SetMusicVolume();
+        SetSFXVolume();
     }
 }
